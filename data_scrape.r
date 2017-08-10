8d12ce37299e84b6796561119f5d4ac4a0e8ec19
Kevin Gardner <kjgardner74@gmail.com>
2017-08-10 14:59:50 -0400

    Replaced write.table with write_csv to fix missing or stray quote in the output file

--- data_scrape.r
+++ data_scrape.r
@@ -1,4 +1,7 @@
 library(tidyverse)
+library(data.table)
+
+options(stringsAsFactors = FALSE)
 
 setwd("~/GitHub/dirt-bikes")
 
@@ -26,80 +29,112 @@ rows_list[[724]] # ...
 rows_list[[725]] # listing 724
 rows_list[[726]] # close table
 
-rows_list[[1]] <- rows_list[[726]] <- NULL # invalid rows
+rows_list[[1]] <- rows_list[[726]] <- NULL # remove invalid rows
 
+cleanup <- function(my.list, i, j, k=1){
+    my.list[[i]] %>% 
+        # split string into columns
+        str_split(.,'</td>') %>% .[[1]] %>% 
+        # get jth column
+        .[j] %>%
+        # remove leading characters
+        str_replace(".*\">", "") %>% 
+        str_replace("<td>", "") %>%
+        str_replace("<tr>", "") %>%
+        # remove trailing characters
+        str_replace("</a>","") %>%
+        # split string into Summary and Full (for description column)
+        str_split("<span>") %>% .[[1]] %>% 
+        # get the kth element (1 for Summary, 2 for Full)
+        .[k] %>% 
+        # remove trailing characters
+        str_replace("</span>","")
+}
 
-rows_list_cleaned <- lapply(X=rows_list, FUN=function(i){
+# cleanup(rows_list, 1, 1)
+# cleanup(rows_list, 1, 2, 1)
+# cleanup(rows_list, 1, 2, 2)
+
+get.url <- function(my.list, i, j){
+    my.list[[i]] %>% 
+        # split string into columns
+        str_split(.,'</td>') %>% .[[1]] %>% 
+        # get jth column
+        .[j] %>% 
+        # remove leading characters
+        str_replace(".*a id=\"", "") %>% 
+        # remove trailing characters
+        str_split('\"') %>% .[[1]] %>% .[1] %>% 
+        # append prefix
+        paste0("http://www.bikefinds.com/for-sale/", .)
+}
+
+# get.url(rows_list, 1, 2)
+
+rows_list_cleaned <- lapply(X=1:length(rows_list), FUN=function(i){
     
     # browser() # use for debugging
     
     data.frame(
-        Bike = i %>% 
-            # Get the first column
-            str_split(.,'</td>') %>% .[[1]] %>% .[1] %>% 
-            str_replace(".*\">", "") %>% 
-            str_replace("</a>",""),
-        
-        Description = i %>% 
-            # Get the second column
-            str_split(.,'</td>') %>% .[[1]] %>% .[2] %>% 
-            str_replace(".*\">", "") %>%
-            str_split("</a>") %>% .[[1]] %>% .[1],
-        
-        # Description = i %>% 
-        #     # Get the second column (Full Description)
-        #     str_split(.,'</td>') %>% .[[1]] %>% .[2] %>%
-        #     str_replace(".*\">", "") %>%
-        #     str_split("</a>") %>% .[[1]] %>% .[2] %>%
-        #     str_replace("<span>","") %>%
-        #     str_replace("</span>",""),
-        
-        Price = i %>% 
-            # get the third column
-            str_split(.,'</td>') %>% .[[1]] %>% .[3] %>%
-            str_replace("<td>",""),
-        
-        Year = i %>%
-            # get the 4th column
-            str_split(.,'</td>') %>% .[[1]] %>% .[4] %>%
-            str_replace("<td>",""),
-        
-        Location = i %>% 
-            # get the 5th column
-            str_split(.,'</td>') %>% .[[1]] %>% .[5] %>% 
-            str_replace("<td>",""),
-        
-        State = i %>% 
-            # get the 6th column
-            str_split(.,'</td>') %>% .[[1]] %>% .[6] %>% 
-            str_replace(".*\">", "") %>% 
-            str_replace("</a>", ""),
-        
-        Listed = i %>%
-            # get the 7th column
-            str_split(.,'</td>') %>% .[[1]] %>% .[7] %>% 
-            str_replace(".*\">", ""),
-        
-        Source = i %>% 
-            # get the 8th column
-            str_split(.,'</td>') %>% .[[1]] %>% .[8] %>% 
-            str_replace("<td>", "")
-        
-        )
-    
+        Bike         = cleanup(rows_list, i, 1),
+        Description1 = cleanup(rows_list, i, 2, 1),
+        Description2 = cleanup(rows_list, i, 2, 2),
+        Price        = cleanup(rows_list, i, 3),
+        Year         = cleanup(rows_list, i, 4),
+        Location     = cleanup(rows_list, i, 5),
+        State        = cleanup(rows_list, i, 6),
+        Listed       = cleanup(rows_list, i, 7),
+        Source       = cleanup(rows_list, i, 8),
+        url          = get.url(rows_list, i, 2))
     })
 
 # rbind list into single data frame
 df <- rows_list_cleaned %>% do.call("rbind",.)
 
-# Frequency table of Bike
-table(df$Bike)[order(-table(df$Bike))]
-# Frequency table of Year
-table(df$Year)[order(-table(df$Year))]
-# Frequency table of State
-table(df$State)[order(-table(df$State))]
-# Frequency table of Source
-table(df$Source)[order(-table(df$Source))]
+problem <- df[df$url=="http://www.bikefinds.com/for-sale/73dab565d760b69da5215106bbf2ec46","Description2"]
+notproblem <- df[df$url=="http://www.bikefinds.com/for-sale/efc285764f1dfc3eeff3fb38009546c0","Description2"]
+lapply(df, class)
+# df <- lapply(df, as.character)
+
+
+# # Frequency table of Bike
+# table(df$Bike)[order(-table(df$Bike))]
+# # Frequency table of Year
+# table(df$Year)[order(-table(df$Year))]
+# # Frequency table of State
+# table(df$State)[order(-table(df$State))]
+# # Frequency table of Source
+# table(df$Source)[order(-table(df$Source))]
 
 # Write cleaned data frame to csv
+write.table(
+    x       = df,
+    file    = "2-stroke_dirt-bikes_cleaned.csv",
+    # file    = stdout(),
+    qmethod = "escape",
+    quote   = TRUE,
+    row.names = FALSE
+    )
+
+length(write.table(
+    x       = problem,
+    # file    = "2-stroke_dirt-bikes_cleaned.csv",
+    file    = stdout(),
+    qmethod = "escape",
+    quote   = TRUE, 
+    row.names = FALSE
+))
+write.table(
+    x       = notproblem,
+    # file    = "2-stroke_dirt-bikes_cleaned.csv",
+    file    = stdout(),
+    qmethod = "escape",
+    quote   = TRUE, 
+    row.names = FALSE
+)
+
+
 write.table(df, file="2-stroke_dirt-bikes_cleaned.csv", row.names = FALSE, col.names = TRUE, sep=",")
+
+
+write_csv(x=df, path="2-stroke_dirt-bikes_cleaned.csv", 
